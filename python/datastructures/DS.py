e17862bc621efca92ffad4b64afaef7534d21aca
soumyakantidas <soumyakantidas007@gmail.com>
2018-04-12 15:23:35 +0530

    Added Queue class

--- python/datastructures/DS.py
+++ python/datastructures/DS.py
@@ -22,3 +22,29 @@ class Stack:
 
     def is_not_empty(self):
         return not self.is_empty()
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.insert(0, value)
+
+    def dequeue(self):
+        return self.queue.pop()
+
+    def size(self):
+        return len(self.queue)
+
+    def is_empty(self):
+        return True if self.size() == 0 else False
+
+    def is_not_empty(self):
+        return not self.is_empty()
+
+    def peek(self):
+        return self.queue[-1]
+
+    def clear(self):
+        self.queue = []
