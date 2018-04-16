c498376449794ab54a6685a0f5530478e7d5f5dd
soumyakantidas <soumyakantidas007@gmail.com>
2018-04-16 13:15:07 +0530

    Implemented and tested Linked list using Node

--- python/datastructures/DS.py
+++ python/datastructures/DS.py
@@ -48,3 +48,72 @@ class Queue:
 
     def clear(self):
         self.queue = []
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return True if self.head is None else False
+
+    def is_not_empty(self):
+        return not self.is_empty()
+
+    def add(self, value):
+        temp = Node(value)
+        temp.next = self.head
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.next
+
+        return count
+
+    def search(self, key):
+        current = self.head
+        found = False
+        while not found and current is not None:
+            if current.value == key:
+                found = True
+                break
+            else:
+                current = current.next
+
+        return found
+
+    def remove(self, key):
+        previous = None
+        current = self.head
+        found = False
+        while not found and current is not None:
+            if current.value == key:
+                found = True
+                break
+            else:
+                previous = current
+                current = current.next
+
+        if found:
+            previous.next = current.next
+
+
+
+    def __str__(self):
+        li = []
+        current = self.head
+        while current is not None:
+            li.append(current.value)
+            current = current.next
+
+        return " -> ".join(map(str, li))
