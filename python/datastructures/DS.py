7b4d316e7ff4acc2efbc492e0cbdc6704a67fb19
soumyakantidas <soumyakantidas007@gmail.com>
2018-04-27 12:56:32 +0530

    Implemented min heap

--- python/datastructures/DS.py
+++ python/datastructures/DS.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Stack:
     def __init__(self):
         self.stack = []
@@ -137,3 +139,159 @@ class LinkedList:
             current = current.next
 
         return " -> ".join(map(str, li))
+
+
+class BinaryNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        if self.left is None and self.right is None:
+            return str(self.value)
+        if self.left is None and self.right is not None:
+            return "{} --> {}".format(self.value, self.right.value)
+        if self.left is not None and self.right is None:
+            return "{} <-- {}".format(self.left.value, self.value)
+        if self.left is not None and self.right is not None:
+            return "{} <-- {} --> {}".format(self.left.value, self.value, self.right.value)
+
+
+class BinaryTree:
+    def __init__(self, value):
+        self.root = BinaryNode(value)
+
+    def insert_left(self, value):
+        if self.root.left is None:
+            self.root.left = BinaryNode(value)
+        else:
+            temp_node = BinaryNode(value)
+            temp_node.left = self.root.left
+            self.root.left = temp_node
+
+    def insert_right(self, value):
+        if self.root.right is None:
+            self.root.right = BinaryNode(value)
+        else:
+            temp_node = BinaryNode(value)
+            temp_node.right = self.root.right
+            self.root.right = temp_node
+
+    def _add(self, value):
+        current = self.root
+        while current is not None:
+            if value < current.value:
+                if current.left is None:
+                    current.left = BinaryNode(value)
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right is None:
+                    current.right = BinaryNode(value)
+                    break
+                else:
+                    current = current.right
+
+    def add(self, value):
+        current = self.root
+
+        while current is not None:
+            if current.left is None and current.right is None:
+                choice = randint(0, 1)
+                if choice == 0:
+                    current.left = BinaryNode(value)
+                    break
+                else:
+                    current.right = BinaryNode(value)
+                    break
+            elif current.left is None:
+                current.left = BinaryNode(value)
+                break
+            elif current.right is None:
+                current.right = BinaryNode(value)
+                break
+            else:
+                choice = randint(0, 1)
+                if choice == 0:
+                    current = current.left
+                else:
+                    current = current.right
+
+    def preorder(self):
+        print(self.root.value)
+        if self.root.left:
+            self.root.left.preorder()
+        if self.root.right:
+            self.root.right.preorder()
+
+    def __str__(self):
+        result = []
+        current = self.root
+        result.append(current.__str__())
+        while current.left is not None:
+            current = current.left
+            result.append(current.__str__())
+        current = self.root
+        while current.right is not None:
+            current = current.right
+            result.append(current.__str__())
+        return "\n".join(result)
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = [0]
+        self.size = 0
+
+    def _percolate_up(self):
+        i = self.size
+        while i // 2 > 0:
+            if self.heap[i//2] > self.heap[i]:
+                self.heap[i//2], self.heap[i] = self.heap[i], self.heap[i//2]
+            i //= 2
+
+    def _percolate_down(self, i):
+        while 2*i <= self.size:
+            mc = self._min_child(i)
+            if self.heap[i] > self.heap[mc]:
+                self.heap[i], self.heap[mc] = self.heap[mc], self.heap[i]
+            i = mc
+
+    def _min_child(self, i):
+        if 2*i+1 > self.size:
+            return 2*i
+        else:
+            if self.heap[2*i] < self.heap[2*i+1]:
+                return 2*i
+            else:
+                return 2*i+1
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.size += 1
+        self._percolate_up()
+
+    def peek(self):
+        return self.heap[1]
+
+    def remove(self):
+        if self.size >= 1:
+            result = self.heap[1]
+            self.heap[1] = self.heap[-1]
+            self.size -= 1
+            self.heap.pop()
+            self._percolate_down(1)
+            return result
+
+    def heapify(self, array):
+        i = len(array) // 2
+        self.heap = [0] + array[:]
+        self.size = len(array)
+        while i > 0:
+            self._percolate_down(i)
+            i -= 1
+
+    def __str__(self):
+        return " ".join(map(str, self.heap[1:]))
--- python/datastructures/DSTest.py
+++ python/datastructures/DSTest.py
@@ -1,4 +1,5 @@
-import python.datastructures.DS as ds
+from python.datastructures.DS import BinaryTree, MinHeap
+from random import randint, seed
 
 # s1 = ds.Stack()
 # print(s1.is_empty())
@@ -11,18 +12,36 @@ import python.datastructures.DS as ds
 # print(s1.pop())
 # print(s1.stack)
 
-l1 = ds.LinkedList()
-l1.add(31)
-l1.add(77)
-l1.add(17)
-l1.add(93)
-l1.add(26)
-l1.add(54)
+# l1 = ds.LinkedList()
+# l1.add(31)
+# l1.add(77)
+# l1.add(17)
+# l1.add(93)
+# l1.add(26)
+# l1.add(54)
+#
+# print(l1)
+# print(l1.size())
+# print(l1.search(76))
+# # l1.remove(93)
+# l1.reverse()
+# print("reversed:", l1)
 
-print(l1)
-print(l1.size())
-print(l1.search(76))
-# l1.remove(93)
-l1.reverse()
-print("reversed:", l1)
+# bt = BinaryTree(50)
+# seed(1)
+#
+# for _ in range(50):
+#     value = randint(0, 100)
+#     # print(value)
+#     bt.add(value)
+#
+# print(bt)
+#
+# bt.preorder()
 
+mh = MinHeap()
+mh.heapify([randint(0, 100) for _ in range(100)])
+# mh.insert()
+# mh.remove()
+print(mh)
+print([mh.remove() for _ in range(101)])
